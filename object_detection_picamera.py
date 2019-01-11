8442aa78cb2c7ba28cfbdd2c51c7a6f4da261dcb
coblin7 <42255950+coblin7@users.noreply.github.com>
2019-01-11 16:11:56 +0900

    Update object_detection_picamera.py

--- object_detection_picamera.py
+++ object_detection_picamera.py
@@ -35,7 +35,7 @@ import numpy as np
 from picamera.array import PiRGBArray
 # (1) 파이카메라 모듈을 불러옵니다. 카메라 출력물로부터 n차원 numpy 배열을 구성하는 클래스를 제공합니다.
 # 이 모듈은 자동으로 import되지 않기 때문에, 명시적으로 import 해주어야 합니다.
-# 카메라의 데이터를 통해 배열을 구성하기 위해서 명시적으로 위 모듈을 불러와 주어야 한다.
+# 카메라의 데이터를 통해 배열을 구성하기 위해서 명시적으로 위 모듈을 불러와 주어야 합니다.
 # (2) picamera.array 모듈에서 PiRGBArray 함수를 불러옵니다.
 # 위 함수는 RGB 캡처를 통해 3차원 RGB 배열(행,열,색상)을 생성합니다.
 ## https://picamera.readthedocs.io/en/release-1.10/api_array.html
@@ -83,11 +83,11 @@ parser.add_argument('--usbcam', help='Use a USB webcam instead of picamera',
 args = parser.parse_args()
 # parse_args() 메소드를 통해 인자를 파싱합니다. 
 # 명령행을 검사하고 인자를 적절한 형태로 변환하여 적절한 액션을 호출하게 됩니다.
-# parse_args() = 인자 파싱하기
+## parse_args() = 인자 파싱하기
 
 if args.usbcam:
     camera_type = 'usb'
-# 명령문에 usbcam 인자가 있다면, camera_type변수는 usb 값을 가지게 됩니다.
+## 명령문에 usbcam 인자가 있다면, camera_type변수는 usb 값을 가지게 됩니다.
 
 # This is needed since the working directory is the object_detection folder.
 sys.path.append('..')
@@ -100,32 +100,32 @@ from utils import label_map_util
 # utilites는 기본적인 영상처리 작업을 수행할 수 있는 모듈입니다.
 # label_map_util에 대한 정보 찾을 수 없음.
 from utils import visualization_utils as vis_util
-# visualization_utils는 Python에서 기계학습을 위한 시각화 유틸리티 기능입니다. 
+## visualization_utils는 Python에서 기계학습을 위한 시각화 유틸리티 기능입니다. 
 
 # Name of the directory containing the object detection module we're using
 MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
 # object detectio 모듈에 사용될 모델의 이름이다.
-# ssd는 single shot detector를 뜻하며, lite버전으로 mobilenet 모바일에서 사용되는 coco모델이다.
+## ssd는 single shot detector를 뜻하며, lite버전으로 mobilenet 모바일에서 사용되는 coco모델이다.
 # ssd(Single Shot Detector), Yolo, RCN 들은 detector중 하나이다.
 
 # Grab path to current working directory
 # 현재의 작업 디렉토리의 경로를 파악합니다.
 CWD_PATH = os.getcwd()
-# CWD_PATH변수에 os.getcwd()는 현재 자신의 디렉토리 위치를 리턴합니다.
+## CWD_PATH변수에 os.getcwd()는 현재 자신의 디렉토리 위치를 리턴합니다.
 
 # Path to frozen detection graph .pb file, which contains the model that is used
 # for object detection.
-# object detection을 위해서 경로에 있는 frozen_detection_graph.pb 파일안에 있는 모델을 사용합니다.
+## object detection을 위해서 경로에 있는 frozen_detection_graph.pb 파일안에 있는 모델을 사용합니다.
 PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
 
 # Path to label map file
 # label의 맵 파일의 경로를 지정합니다.
 PATH_TO_LABELS = os.path.join(CWD_PATH,'data','mscoco_label_map.pbtxt')
-# os.path.join()은 경로를 병합하여 새로운 경로를 생성합니다. PATH_TO_LABELS 변수에 새로운 경로를 삽입합니다.
+## os.path.join()은 경로를 병합하여 새로운 경로를 생성합니다. PATH_TO_LABELS 변수에 새로운 경로를 삽입합니다.
 
 
 # Number of classes the object detector can identify
-# object detector가 식별할 수 있는 클래스의 수를 나타냅니다.
+## object detector가 식별할 수 있는 클래스의 수를 나타냅니다.
 NUM_CLASSES = 90
 
 ## Load the label map.
@@ -139,53 +139,92 @@ categories = label_map_util.convert_label_map_to_categories(label_map, max_num_c
 category_index = label_map_util.create_category_index(categories)
 
 # Load the Tensorflow model into memory.
-# 텐서플로우를 통해 그래프안에 데이터를 넣어 영상처럼 그려줍니다.
-# 뉴런네트워크, 딥러닝 초기강좌
+# 텐서플로우를 통해 그래프안에 데이터를 넣어 영상처럼 그려줍니다. (계산의 데이터 흐름을 그래프료 표시합니다.)
 detection_graph = tf.Graph()
+# TensorFlow에서의 계산의 기본은 Graph 객체입니다. 이는 각각의 연산을 나타내고 입력과 출력으로써 서로 연결되어 있는 노드들의 네트워크를 가지고 있다.
+# 기본 Graph는 항상 tf.get_default_graph를 호출하여 등록되고 액세스 할 수 있습니다.
+# 기본그래프에서 연산을 추가하려면 새로운 Operation을 정의하는 함수 중 하나를 호출하면 됩니다. 
+# 아래와 같이 as_default 메소드를 사용하여 현재의 기본 그래프를 재정의 할 수 있습니다.
+## detection_graph 변수에 Graph() 객체를 생성합니다.
+
 with detection_graph.as_default():
+# as_default() 메소드는 Graph를 디폴트 그래프로하는 컨택스트 매니저를 돌려줍니다.
+# 동일한 프로스세에서 여러 개의 그래프를 작성하려는 경우 이 메소드를 사용해야 합니다.
+# 편의상 전역 기본 그래프가 제공되며, 사용자가 새 그래프를 명시적으로 만들지 않으면 모든 그래프가 이 그래프에 추가됩니다.
+# 블록의 범위 내에서 생성된 연산이 이 그래프에 추가되도록 지정하려면 with 키워드와 함께 메소드를 사용하면 됩니다.
     od_graph_def = tf.GraphDef()
+    # GraphDef() 클래스는 기존에 정의된 프로토콜 버퍼 라이브러리에 의해 생성되는 객체입니다.
+    # 프로토콜 버퍼 툴은 텍스트 파일을 파싱하고 그래프 정의를 로딩, 저장 및 조작하는 코드를 생성합니다.
+    ## od_graph_def 에는 graph.proto에 텍스트로 정의되며 생성된 GraphDef 클래스의 빈 객체를 생성하였습니다.
     with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    ## 모델의 경로를 설정해주었던 파일을 'rb'(read binary)하여 이미지 파일의 데이터를 od_graph_def 객체에 채워줍니다.
+    # 프로토콜 버퍼가 저장할 수 있는 포맷은 두 가지 형태 텍스트 포맷과 바이너리 포맷이 있습니다.
+    # 텍스트 포맷은 사람이 읽을 수 있는 형태이며, 디버깅과 편집이 편리하지만 가중치자와 같은 수치 데이터가 저장될 경우 커질 수 있습니다.
+    # 바이너리 포맷은 읽기는 어렵지만, 같은 내용의 텍스트 포맷보다 훨씬 작은 크기를 갖습니다. 
+    # 텍스트 파일을 로드할 때는 text_format 모듈에 있는 유틸리티 함수를 사용합니다.
+    # 바이너리 파일을 로드할 때는 ParseFromString()을 사용합니다.
         serialized_graph = fid.read()
+        ## fid.read()는 파일에서 전체 내용 또는 일부를 읽기 위한 메소드입니다. (파일읽기)
         od_graph_def.ParseFromString(serialized_graph)
+        ## 경로를 설정하고 텐서플로우 객체 파일을 넣어주었던 od_graph_def 인스턴스에 ParseFromString()을 통해 호출하여 serialized_graph 파일읽기 메소드를 수행합니다. 
         tf.import_graph_def(od_graph_def, name='')
-
+        ## tf.import_graph_def를 사용하여 여러개의 tensorflow 모델을 연결하여 하나의 그래프로 결합합니다.
+        # 그러나 기존 그래프 중 하나에 변수가 포함되어 있으면 새 그래프에 값을 삽입할 수 없습니다. 
     sess = tf.Session(graph=detection_graph)
-
+    # Session 객체는 Operation 객체가 실행되고 Tensor 객체가 계산되는 환경을 캡슐화 합니다. (실행환경을 캡슐화 한 것)
+    # Session은 각종 자원 위에 그래프를 올려 놓아 실행시켜줍니다.
+    ## Session 클래스의 객체를 생성합니다.
 
 # Define input and output tensors (i.e. data) for the object detection classifier
+# object detction 분류를 위한 텐서(데이터) 입출력 정의
 
 # Input tensor is the image
+# 이미지 텐서를 입력합니다.
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+## get_tensor_by_name은 placeholder를 불러옵니다.
+# placeholder는 Tensorflow의 특이한 자료형이다. 선언과 동시에 초기화 하는 것이 아닌 일단 선언 후 값을 전달해 주어야 한다. 
+# 따라서 반드시 실행 시 데이터가 제공되어야 한다.
+# 여기서 값 전달이란, 데이터나 상수 값을 할당하는 것이 아닌 Tensor를 placeholder에 맵핑 시키는 것이라고 보면 된다.
+## detection_graph의 'image_tensor:0'이라는 이름의 tensor를 placeholder에 맵핑하여 image_tensor 변수안에 넣은 것이다.
+
 
 # Output tensors are the detection boxes, scores, and classes
+# 입력된 이미지를 통해 박스, 점수, 클래스의 Tensor를 출력합니다.
 # Each box represents a part of the image where a particular object was detected
+# 각 상자는 특정 물체가 감지된 이미지의 일부를 나타냅니다.
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
 # Each score represents level of confidence for each of the objects.
+# 각 점수는 각 분류된 객체에 대한 신뢰도 점수를 나타냅니다.
 # The score is shown on the result image, together with the class label.
+# 점수는 클래스 라벨과 함께 결과 영상에 표시되게 됩니다.
 detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
 # Number of objects detected
+# 탐지된 개체 수
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-# opencv구역
+# opencv 영역
 # Initialize frame rate calculation
+# 프레임률 계산 초기화
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 # Initialize camera and perform object detection.
+# 카메라 초기화와 물체감지를 수행합니다.
 # The camera has to be set up and used differently depending on if it's a
 # Picamera or USB webcam.
+# 카메라를 설치하고 파이카메라나 usb카메라를 사용합니다.
 
 # I know this is ugly, but I basically copy+pasted the code for the object
 # detection loop twice, and made one work for Picamera and the other work
 # for USB.
 
 ### Picamera ###
-# 카메라에 이미지가 들어오면 박스와 텍스트를 입히는 GUI와 관련된 내용이다.
+# 카메라에 이미지가 들어오면 박스와 텍스트를 입히는 GUI와 관련된 내용입니다.
 if camera_type == 'picamera':
     # Initialize Picamera and grab reference to the raw capture
     camera = PiCamera()
@@ -210,6 +249,7 @@ if camera_type == 'picamera':
             feed_dict={image_tensor: frame_expanded})
 
         # Draw the results of the detection (aka 'visulaize the results')
+        # 인식 결과를 시각적으로 도출합니다.
         vis_util.visualize_boxes_and_labels_on_image_array(
             frame,
             np.squeeze(boxes),
@@ -223,6 +263,7 @@ if camera_type == 'picamera':
         cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
 
         # All the results have been drawn on the frame, so it's time to display it.
+        # 모든 결과가 프레임에 도출되었고 최종적으로 사용자에게 보여줍니다. 
         cv2.imshow('Object detector', frame)
 
         t2 = cv2.getTickCount()
