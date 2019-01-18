d4b5d25672d446dece09f9493666bc6a9f409ec8
coblin7 <42255950+coblin7@users.noreply.github.com>
2019-01-18 22:46:37 +0900

    Update OpenCV를 통해 비디오 출력하기.py

--- OpenCV를 통해 비디오 출력하기.py
+++ OpenCV를 통해 비디오 출력하기.py
@@ -1,18 +1,24 @@
+# with pycamera
+
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from time import sleep
 
-# 카메라에 접근하기 위해 videocapture 객체를 생성
-# 만약 비디오가 2개면 인자를 1로 주면됩니다.
-cap = cv2.VideoCapture(0)
+camera = PiCamera()
+camera.resolution = (480,320)
+camera.framerate = 32
+# 카메라 변수에 설정했던 정보를 PiRGBArray 함수를 통해 메모리 배열을 구성합니다. (numpy.ndarray)
+rawCapture = PiRGBArray(camera, size=(480,320))
+sleep(0.1)
 
-while(True):
-  ret, img_color = cap.read()
-  if ret == false:
-    continue
-  cv2.imshow("Color", img_color)
-  if cv2.waitKey(1):
+# continuous 함수를 통해 계속해서 캡쳐를 하며 이를 frame 변수에 저장 합니다.
+for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+  image = frame.array
+  
+  cv2.imshow('Frame', image)
+  key=cv2.waitKey(1) & 0xFF
+  rawCapture.truncate(0)
+  if key == 27:
     break
-
-cap.release()
-cv2.destroyAllWindows()
-  
-  
+    
